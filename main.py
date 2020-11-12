a20a9a3ddd928b4c711c95c390a5eff85295176f
Janghyeon Lee <janghyeon300@gmail.com>
2020-11-12 22:39:33 +0900

    simple error fixed by janghyeon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 ##from indeed import get_jobs as get_indeed_jobs
 ##from so import get_jobs as get_so_jobs
 from exporter import save_to_file
